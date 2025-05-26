194a3a99046adf002a6624ad8b969139a144b971
Krunal Bende <96563861+Coderkreet@users.noreply.github.com>
2025-05-26 23:56:03 +0530

    Refer by kreet

---
+++ src/constants/content/MainContent.js
@@ -0,0 +1,27 @@
+// import appLogo from "../../assets/app/appLogo.jpg";
+import appLogoClr from "../../assets/app/appLogoTran.png";
+import icon from "../../assets/app/icon.png";
+export const MainContent = {
+  appName: "NEXOCOIN",
+  appLogo: appLogoClr,
+  appLogoClr: appLogoClr,
+  appURL: "",
+  contactNo: "+9876324862552",
+  email: "info@example.com",
+  address: "Address, City, Country",
+  telegram_link: "https://t.me/nexochann",
+  favIcon: icon,
+};
+
+export const backendConfig = {
+  // base: "http://192.168.29.221:3000/api",
+  // origin: "http://192.168.29.221:3000",
+  // base: "https://api.bitblock.network/api",
+  // origin: "https://api.bitblock.network",
+  // base: "http://192.168.3.56:6018/api",
+  // origin: "http://192.168.3.56:6018",
+  // base: "http://192.168.190.56:6018/api",
+  // origin: "http://192.168.190.56:6018",
+  base: "https://api.nexoinvest.org/api",
+  origin: "https://api.nexoinvest.org",
+};
---
+++ src/screens/admin/UpdateContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateContent = () => {
+  return (
+    <div>
+      all content update here 
+    </div>
+  )
+}
+
+export default UpdateContent
